ef07e923a309b6a93534a8c0ef7d580f3b13c501
Mednoob <ahmadyaqdhan123@gmail.com>
2020-07-26 23:42:41 +0900

    Added uptime command and a package. Wrapped up explanation coming soon!

--- Javascript/Eris/index.js
+++ Javascript/Eris/index.js
@@ -1,53 +1,63 @@
-const eris = require("eris");                         //Eris | Required as a Lib/Wrapper for the javascript code
-const { token } = require("./Config/Token.json");    //Token | Required for logging in the bot
-const { prefix } = require("./Config/Prefix.json"); //prefix | Required to call the bot
-const { Message } = require("eris");               //Message | Dunno where is this thing from. It's automatically inside this file
-
-
-if(token === "") {
-    console.log("Please, put the bot's token in the Token.json"); 
-    return;
-} //Cancel out the current logging attempt if the bot's token is null
-
-if(prefix === "") {
-    console.log("Please, put the bot's prefix in the Prefix.json");
-    return;
-} //Cancel out the current logging attempt if the bot's prefix is null
-
-
-var client = new eris(token); //New connection client made using eris with the Bot's Token
-client.on("ready", () => { //When the bot is ready
-    console.log("Bot is ready!"); //Add log in the console: "Bot is ready!"
-}); 
-client.on("messageCreate", (msg) => { 
-    if(!msg.content.startsWith(prefix)) return; //The bot will not respond any message that is not started with its prefix. This code usually use to prevent someone using the bot to spam by just typing non-prefixed message. You may delete this code
-    if(msg.content === prefix + "help") {
-        client.createMessage(msg.channel.id, { //Create a message
-            embed: { //Embed Message Properties
-                title: "Help Command", //Embed Title
-                description: "List of **Commands** of this bot.\nLook **Below**.", //Embed Description. Use "\n" to put the text in a new line
-                author: { //Embed Author Properties
-                    name: msg.author.username, //Embed Author Name
-                    icon_url: msg.author.avatarURL //Embed Author Icon
-                },
-                color: 2, //Embed Color. May be shown in Hex or a Base-10 Integer
-                fields: [ //Embed Fields Array
-                    { //First Embed Field Properties
-                        name: "Basic Commands", //First Embed Field Name
-                        value: "`help`", //First Embed Field Context/Value
-                        inline: true //First Embed Field Inline Boolean. Set to 'true' if you want to put the field in the same line as before. Set to 'false' if you want to put the field in a new line.
-                    },
-                    { //Second Embed Field Properties
-                        name: "More Commands", //Second Embed Field Name
-                        value: "Coming Soon!", //Second Embed Field Context/Value
-                        inline: false //Second Embed Field Inline Boolean. Set to 'true' if you want to put the field in the same line as before. Set to 'false' if you want to put the field in a new line.
-                    }
-                ],
-                footer: { //Embed Footer Properties
-                    text: "Eris Example" //Embed Footer Text
-                }
-            }
-        });
-    }
-});
-client.connect();
+const eris = require("eris");                                                                                                                                                                                   //Eris | Required as a Lib/Wrapper for the javascript code
+const { token } = require("./Config/Token.json");                                                                                                                                                              //Token | Required for logging in the bot
+const { prefix } = require("./Config/Prefix.json");                                                                                                                                                           //Prefix | Required to call the bot
+const { Message } = require("eris");                                                                                                                                                                         //Message | Dunno where is this thing from. It's automatically inside this file
+const prettyms = require("pretty-ms");                                                                                                                                                                       //
+                                                                                                                                                                                                             //
+if(token === "") {                                                                                                                                                                                           //
+    console.log("Please, put the bot's token in the Token.json");                                                                                                                                            //
+    return;                                                                                                                                                                                                  //
+}                                                                                                                                                                                                            //Cancel out the current logging attempt if the bot's token is null
+                                                                                                                                                                                                             //
+if(prefix === "") {                                                                                                                                                                                          //
+    console.log("Please, put the bot's prefix in the Prefix.json");                                                                                                                                          //
+    return;                                                                                                                                                                                                  //
+}                                                                                                                                                                                                            //Cancel out the current logging attempt if the bot's prefix is null
+                                                                                                                                                                                                             //
+                                                                                                                                                                                                             //
+var client = new eris(token);                                                                                                                                                                                //New connection client made using eris with the Bot's Token
+let clientuptime = client.uptime                                                                                                                                                                             //
+client.on("ready", () => {                                                                                                                                                                                   //When the bot is ready
+    console.log(client.user.username + " is ready!");                                                                                                                                                        //Add log in the console: "Bot is ready!"
+});                                                                                                                                                                                                          //
+client.on("messageCreate", (msg) => {                                                                                                                                                                        //
+    if(!msg.content.startsWith(prefix)) return;                                                                                                                                                              //The bot will not respond any message that is not started with its prefix. This code usually use to prevent someone using the bot to spam by just typing non-prefixed message. You may delete this code
+    if(msg.content === prefix + "help") {                                                                                                                                                                    //If the message content is prefix and help
+        client.createMessage(msg.channel.id, {                                                                                                                                                               //Create a message
+            embed: {                                                                                                                                                                                         //Embed Message Properties
+                title: "Help Command",                                                                                                                                                                       //Embed Title
+                description: "List of **Commands** of this bot.\nLook **Below**.",                                                                                                                           //Embed Description. Use "\n" to put the text in a new line
+                author: {                                                                                                                                                                                    //Embed Author Properties
+                    name: msg.author.username,                                                                                                                                                               //Embed Author Name
+                    icon_url: msg.author.avatarURL                                                                                                                                                           //Embed Author Icon
+                },                                                                                                                                                                                           //
+                color: 299646,                                                                                                                                                                               //Embed Color. May be shown in Hex or a Base-10 Integer
+                fields: [                                                                                                                                                                                    //Embed Fields Array
+                    {                                                                                                                                                                                        //First Embed Field Properties
+                        name: "Basic Commands",                                                                                                                                                              //First Embed Field Name
+                        value: "`help`",                                                                                                                                                                     //First Embed Field Context/Value
+                        inline: true                                                                                                                                                                         //First Embed Field Inline Boolean. Set to 'true' if you want to put the field in the same line as before. Set to 'false' if you want to put the field in a new line.
+                    },                                                                                                                                                                                       //
+                    {                                                                                                                                                                                        //Second Embed Field Properties
+                        name: "More Commands",                                                                                                                                                               //Second Embed Field Name
+                        value: "Coming Soon!",                                                                                                                                                               //Second Embed Field Context/Value
+                        inline: false                                                                                                                                                                        //Second Embed Field Inline Boolean. Set to 'true' if you want to put the field in the same line as before. Set to 'false' if you want to put the field in a new line.
+                    }                                                                                                                                                                                        //
+                ],                                                                                                                                                                                           //
+                footer: {                                                                                                                                                                                    //Embed Footer Properties
+                    text: "Eris Example"                                                                                                                                                                     //Embed Footer Text
+                }                                                                                                                                                                                            //
+            }                                                                                                                                                                                                //
+        });                                                                                                                                                                                                  //
+    }                                                                                                                                                                                                        //
+    if(msg.content == prefix + "uptime"){                                                                                                                                                                    //
+        const uptimecmduptime = prettyms(client.uptime, {verbose: true})                                                                                                                                     //
+        client.createMessage(msg.channel.id, {                                                                                                                                                               //
+            embed: {                                                                                                                                                                                         //
+                description: "**Bot Uptime:** " + uptimecmduptime,                                                                                                                                           //
+                color: 299646                                                                                                                                                                                //
+            }                                                                                                                                                                                                //
+        })                                                                                                                                                                                                   //
+    }                                                                                                                                                                                                        //
+});                                                                                                                                                                                                          //
+client.connect();                                                                                                                                                                                            //The bot log in to Discord
--- README.md
+++ README.md
@@ -6,8 +6,21 @@ A Project that contains Basic Discord Bots from various of Languages and Libs/Wr
 **Javascript**
 
  1. Eris
+ 2. Discord.Js
 
+**C#**
+
+1. Discord.Net (Coming Soon. Being typed right now.)
 
 More Languages and Libs **Coming Soon!**
 
-...
+
+# Contribute
+If you want to contribute to this project:
+
+- Fork this repo
+- Edit it on your account
+- Commit it
+- Create pull request
+
+This project welcome all people who wanted to contribute and give their own idea to the project.
